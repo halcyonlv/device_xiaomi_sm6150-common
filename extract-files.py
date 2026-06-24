814937b76a2f77857ef2bd7de401e21cc58ec6c0
binbin323 <bin@dhao.cc>
2026-06-25 01:32:14 +0900

    sm6150-common: Patch wfdservice & wfdmmsrc with libaudiobase
    Change-Id: I8e223fc0448d5a4ec352094108318cda08c7bc82

--- extract-files.py
+++ extract-files.py
@@ -48,8 +48,10 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     ('system_ext/lib/libwfdmmsrc_system.so', 'system_ext/lib64/libwfdmmsrc_system.so'): blob_fixup()
+        .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
